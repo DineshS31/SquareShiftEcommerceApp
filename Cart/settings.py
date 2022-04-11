5ed590d31d29c3edc5c3952a9c82be194038e9f1
DineshS31 <37693155+DineshS31@users.noreply.github.com>
2022-04-11 13:51:37 +0530

    Delete settings.py

--- Cart/settings.py
+++
@@ -1,5 +0,0 @@
-# API URLs
-cart_add_item_url = "https://e-commerce-api-recruitment.herokuapp.com/cart/item"
-cart_list_item_url = "https://e-commerce-api-recruitment.herokuapp.com/cart/items"
-cart_get_total_value_url = "https://e-commerce-api-recruitment.herokuapp.com/cart/checkout-value"
-empty_cart_url = "https://e-commerce-api-recruitment.herokuapp.com/cart/items"
