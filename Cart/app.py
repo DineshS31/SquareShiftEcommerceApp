d14550337b946b89154f2210a9a0e453cfd3b962
DineshS31 <37693155+DineshS31@users.noreply.github.com>
2022-04-11 13:51:04 +0530

    Updated the app based on feedback given

--- Cart/app.py
+++ Cart/app.py
@@ -1,30 +1,58 @@
-import requests
-import settings
 import json
+import os
 from flask import Flask, request, Response
 
-
 app = Flask(__name__)
+
+product_dir = os.path.join(os.path.dirname(__file__), "products.json")
+cart_dir = os.path.join(os.path.dirname(__file__), "cart_items.json")
+warehouse_distance_dir = os.path.join(os.path.dirname(__file__), "warehouse_distance.json")
+products = json.loads(open(product_dir).read())
+
+
+@app.route("/list_products")
+def list_products():
+    return Response(json.dumps({"status": "success", "products": products}),
+                    status=200,
+                    mimetype="application/json")
 
 
 @app.route("/cart/add_item", methods=['POST'])
 def add_item():
     req_payload = request.get_json()
     result = {}
-    status_code = 200
     if "product_id" not in req_payload or "quantity" not in req_payload:
-        result['success'] = False
+        result['status'] = False
         result['message'] = "Product Id & Quantity is required!"
         status_code = 400
     else:
-        url = settings.cart_add_item_url
-        response = requests.request("POST", url, json=req_payload)
-        if response.status_code == 200:
-            result = response.json()
+        product_id = req_payload['product_id']
+        quantity = req_payload['quantity']
+        no_of_items_to_be_added = []
+        for product_info in products:
+            if product_info['id'] == int(product_id):
+                no_of_items_to_be_added.append({
+                    "product_id": int(product_id),
+                    "description": product_info['name'],
+                    "quantity": quantity
+                })
+        if no_of_items_to_be_added:
+            with open(cart_dir, "a+") as outfile:
+                outfile.seek(0)
+                f = outfile.read()
+                if f:
+                    data = json.loads(f)
+                    no_of_items_to_be_added.extend(data)
+                outfile.truncate(0)
+                outfile.write(json.dumps(no_of_items_to_be_added, indent=4))
+            status_code = 201
+            result['status'] = True
+            result['message'] = "Item has been added to cart"
         else:
-            result['success'] = False
-            result['message'] = "Error in adding item to cart!"
-            status_code = 500
+            status_code = 200
+            result['status'] = True
+            result['message'] = "Product not found! try listing products once..."
+
     return Response(json.dumps(result),
                     status=status_code,
                     mimetype="application/json")
@@ -32,70 +60,157 @@ def add_item():
 
 @app.route("/cart/list_items")
 def list_items():
-    url = settings.cart_list_item_url
-    response = requests.request("GET", url)
-    if response.status_code == 200:
-        result = Response(json.dumps(response.json()),
-                          status=response.status_code,
-                          mimetype="application/json")
-    elif response.status_code == 400:
-        result = Response(json.dumps({"success": True, "message": "Cart is empty!"}),
-                          status=response.status_code,
+    cart_items = open(cart_dir).read()
+    if cart_items:
+        cart_items = json.loads(cart_items)
+        response = {
+            "status": "success",
+            "message": "Item available in the cart",
+            "items": cart_items
+        }
+        result = Response(json.dumps(response),
+                          status=200,
                           mimetype="application/json")
     else:
-        result = Response(json.dumps({"success": False, "message": "Error in listing items!"}),
-                          status=response.status_code,
+        result = Response(json.dumps({"message": "Cart is empty!"}),
+                          status=400,
                           mimetype="application/json")
+
     return result
 
 
 @app.route("/cart/get_items_total", methods=['POST'])
 def get_items_total():
-    url = settings.cart_get_total_value_url
     req_payload = request.get_json()
     if "postal_code" not in req_payload:
         result = Response(json.dumps({"success": False, "message": "Postal code is required!"}),
                           status=400,
                           mimetype="application/json")
     else:
-        response = requests.request("GET", url, params={"shipping_postal_code": req_payload['postal_code']})
-        if response.status_code == 200:
-            result = Response(json.dumps(response.json()),
-                              status=response.status_code,
-                              mimetype="application/json")
-        elif response.status_code == 400:
-            result = Response(json.dumps({"success": True, "message": "Cart is empty!"}),
-                              status=response.status_code,
+        postal_code = req_payload['postal_code']
+        if postal_code not in range(465535, 465546):
+            result = Response(json.dumps({"success": False,
+                                          "message": "Invalid postal code, valid ones are 465535 to 465545."}),
+                              status=400,
                               mimetype="application/json")
         else:
-            result = Response(json.dumps({"success": False, "message": "Error in getting items total!"}),
-                              status=response.status_code,
+            warehouse_distance = json.loads(open(warehouse_distance_dir).read())
+            distance_in_km = 0.0
+            for distance_info in warehouse_distance:
+                if distance_info['postal_code'] == postal_code:
+                    distance_in_km = distance_info['distance_in_kilometers']
+                    break
+            price_value = 0
+            cart_items = open(cart_dir).read()
+            if cart_items:
+                cart_items = json.loads(cart_items)
+                for item_info in cart_items:
+                    product_id = item_info['product_id']
+                    product = {}
+                    for product_info in products:
+                        if product_info['id'] == product_id:
+                            product = product_info
+                            break
+                    if product:
+                        price_value += product.get("price", 0) * item_info['quantity']
+                        discount = (price_value * float(product.get('discount_percentage', 0)))/100
+                        price_value -= discount
+                        item_weight_including_quantity = float(product.get("weight_in_grams", 0)) * item_info['quantity']
+                        distance_price_value = calculate_distance(item_weight_including_quantity, distance_in_km,
+                                                                  price_value)
+                        price_value += distance_price_value
+
+                response = {
+                    "status": "success",
+                    "message": f"Total value of your shopping cart is - ${price_value}"
+                }
+            else:
+                response = {
+                    "status": "success",
+                    "message": "No Items has been to your cart!"
+                }
+            result = Response(json.dumps(response),
+                              status=200,
                               mimetype="application/json")
+
     return result
+
+
+def calculate_distance(weight_in_gms, distance_in_km, price_value):
+    grams = float(weight_in_gms)
+    kilograms = grams / 1000
+    distance_price_value = 0.0
+    if kilograms < 2 and distance_in_km < 5:
+        distance_price_value = 12
+    elif kilograms < 2 and (5 < distance_in_km < 20):
+        distance_price_value = 15
+    elif kilograms < 2 and (20 < distance_in_km < 50):
+        distance_price_value = 20
+    elif kilograms < 2 and (50 < distance_in_km < 500):
+        distance_price_value = 50
+    elif kilograms < 2 and (500 < distance_in_km < 800):
+        distance_price_value = 100
+    elif kilograms < 2 and distance_in_km > 800:
+        distance_price_value = 220
+
+    elif (2.01 < kilograms < 5) and distance_in_km < 5:
+        distance_price_value = 14
+    elif (2.01 < kilograms < 5) and (5 < distance_in_km < 20):
+        distance_price_value = 18
+    elif (2.01 < kilograms < 5) and (20 < distance_in_km < 50):
+        distance_price_value = 24
+    elif (2.01 < kilograms < 5) and (50 < distance_in_km < 500):
+        distance_price_value = 55
+    elif (2.01 < kilograms < 5) and (500 < distance_in_km < 800):
+        distance_price_value = 110
+    elif (2.01 < kilograms < 5) and distance_in_km > 800:
+        distance_price_value = 250
+
+    elif (5.01 < kilograms < 20) and distance_in_km < 5:
+        distance_price_value = 16
+    elif (5.01 < kilograms < 20) and (5 < distance_in_km < 20):
+        distance_price_value = 25
+    elif (5.01 < kilograms < 20) and (20 < distance_in_km < 50):
+        distance_price_value = 30
+    elif (5.01 < kilograms < 20) and (50 < distance_in_km < 500):
+        distance_price_value = 80
+    elif (5.01 < kilograms < 20) and (500 < distance_in_km < 800):
+        distance_price_value = 130
+    elif (5.01 < kilograms < 20) and distance_in_km > 800:
+        distance_price_value = 270
+
+    elif kilograms > 20.01 and distance_in_km < 5:
+        distance_price_value = 21
+    elif kilograms > 20.01 and (5 < distance_in_km < 20):
+        distance_price_value = 35
+    elif kilograms > 20.01 and (20 < distance_in_km < 50):
+        distance_price_value = 50
+    elif kilograms > 20.01 and (50 < distance_in_km < 500):
+        distance_price_value = 90
+    elif kilograms > 20.01 and (500 < distance_in_km < 800):
+        distance_price_value = 150
+    elif kilograms > 20.01 and distance_in_km > 800:
+        distance_price_value = 300
+
+    item_price_with_distance = distance_price_value + price_value
+    return item_price_with_distance
 
 
 @app.route("/cart/empty_cart", methods=['POST'])
 def empty_cart():
-    url = settings.empty_cart_url
     req_payload = request.get_json()
     if "action" not in req_payload:
         result = Response(json.dumps({"success": False, "message": "Action is required!"}),
                           status=400,
                           mimetype="application/json")
     else:
-        response = requests.request("POST", url)
-        if response.status_code == 200:
-            result = Response(json.dumps(response.json()),
-                              status=response.status_code,
-                              mimetype="application/json")
-        elif response.status_code == 400:
-            result = Response(json.dumps({"success": True, "message": "Bad request!"}),
-                              status=response.status_code,
-                              mimetype="application/json")
-        else:
-            result = Response(json.dumps({"success": False, "message": "Error in removing items from cart!"}),
-                              status=response.status_code,
-                              mimetype="application/json")
+        with open(cart_dir, "w") as outfile:
+            pass
+
+        result = Response(json.dumps({"status": "success",
+                                      "message": "All items have been removed from the cart !"}),
+                          status=200,
+                          mimetype="application/json")
     return result
 
 
